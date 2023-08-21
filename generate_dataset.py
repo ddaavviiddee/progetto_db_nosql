0157a4e4872b33d3221c4e8400081c802b47797d
ddaavviiddee <davidemento02@gmail.com>
2023-08-21 02:09:57 +0200

    Il db ha le relazioni , sistemare num entità

--- generate_dataset.py
+++ generate_dataset.py
@@ -89,57 +89,45 @@ def clear_csv(file_name):
     with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
         pass 
 
-entities = {
-    "clients": generate_clients,
-    "merchants" : generate_merchants,
-    "transactions" : generate_transactions,
-    "suspicious_transactions" : generate_suspicious_transactions,
-    "fraud_alerts": generate_fraud_alerts
-}
-
-# ... (codice precedente)
 
 db_sizes = [0.25, 0.50, 0.75, 1.00]
-max_records = 10000
+max_records = 1000
 clients = generate_clients(max_records)
 merchants = generate_merchants(max_records)
 transactions = generate_transactions(max_records, clients, merchants)
 suspicious_transactions = generate_suspicious_transactions(max_records, clients, transactions)
+fraud_alerts = generate_fraud_alerts(max_records, suspicious_transactions)
+
+entities = {
+    "clients": clients,
+    "merchants": merchants,
+    "transactions": transactions,
+    "suspicious_transactions": suspicious_transactions,
+    "fraud_alerts": fraud_alerts
+}
 
 entity_proportions = {
     "clients": 2,
     "merchants": 1,
-    "transactions": 3,
-    "suspicious_transactions": 2,
+    "transactions": 4,
+    "suspicious_transactions": 1,
     "fraud_alerts": 1
 }
 
-for entity_name, generate_function in entities.items():
-    for size in db_sizes:
-        max_records_per_entity = int(size * max_records)  # Numero massimo di record per l'entità in base alla dimensione
-        
-        # Calcola il numero effettivo di record per l'entità proporzionata
-        num_records = int(max_records_per_entity * entity_proportions[entity_name])
+db_sizes = [0.25, 0.50, 0.75, 1.00]
 
-        if entity_name == "transactions":
-            print("generating transactions")
-            data = generate_function(num_records, clients, merchants)
-        elif entity_name == "suspicious_transactions":
-            print("generating sus transactions")
-            data = generate_function(num_records, clients, transactions)
-        elif entity_name == "fraud_alerts":
-            print("generating fraud alerts")
-            data = generate_function(num_records, suspicious_transactions)
-        else:
-            data = generate_function(num_records)
-        
-        # Crea il percorso della cartella basato sulla dimensione del DB
+for entity_name, data in entities.items():
+    for size in db_sizes:
+        max_records_per_entity = int(size * max_records)
+        num_records = int(max_records_per_entity / entity_proportions[entity_name])
+
+        data_subset = data[:num_records]  # Prendi una sottoinsieme dei dati
+
         db_folder = f"db_{int(size * 100)}"
-        os.makedirs(db_folder, exist_ok=True)  # Crea la cartella se non esiste
+        os.makedirs(db_folder, exist_ok=True)
         
         file_name = f"{entity_name}_{int(size * 100)}.csv"
-        file_path = os.path.join(db_folder, file_name)  # Percorso completo del file
+        file_path = os.path.join(db_folder, file_name)
         
-        generate_csv(data, file_path)
-        
+        generate_csv(data_subset, file_path)
         print(f"Generated records for {entity_name} in {file_path}")
